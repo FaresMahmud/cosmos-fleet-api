aa2f6898a4c84e7fb47f4c449ffe9b3f3856e69e
faresa13 <fares13mahmud@outlook.com>
2026-05-05 16:48:04 -0300

    Configura projeto Spring Boot da frota

---
+++ src/main/java/com/cosmos/fleet/CosmosFleetApiApplication.java
@@ -0,0 +1,13 @@
+package com.cosmos.fleet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosFleetApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CosmosFleetApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cosmos/fleet/CosmosFleetApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cosmos.fleet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosFleetApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
